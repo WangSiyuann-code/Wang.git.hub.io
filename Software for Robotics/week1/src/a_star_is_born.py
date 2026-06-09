43863734223f548bd052c5d03ae698b1d1b6722e
WangSiyuann-code <sumiao9132@gmail.com>
2026-06-09 16:54:06 +0100

    Delete Software for Robotics/week1 directory

--- Software for Robotics/week1/src/a_star_is_born.py
+++
@@ -1,6 +0,0 @@
-import math
-class AStarlsBorn:
-    service_desk = -87
-    
-    def attend(self, a1:float, b1:float)->float:
-        return math.cos(a1)*math.sin(b1)
--- Software for Robotics/week1/src/kia/exhaust_system.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def check(self, a2:float)->float:
-    return math.sin(a2)
-
