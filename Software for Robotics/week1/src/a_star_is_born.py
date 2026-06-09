2dc7050a5f146e5f826d7a335dd638e6a7c4fa7c
WangSiyuann-code <sumiao9132@gmail.com>
2026-06-09 16:48:34 +0100

    Add files via upload

---
+++ Software for Robotics/week1/src/a_star_is_born.py
@@ -0,0 +1,6 @@
+import math
+class AStarlsBorn:
+    service_desk = -87
+    
+    def attend(self, a1:float, b1:float)->float:
+        return math.cos(a1)*math.sin(b1)
---
+++ Software for Robotics/week1/src/kia/exhaust_system.py
@@ -0,0 +1,5 @@
+import math
+
+def check(self, a2:float)->float:
+    return math.sin(a2)
+
